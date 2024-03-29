be626ff1824ec7694bf9ffd4755d57f5b09575ef
Jayson Hundsrucker <hundsruckerjayson@gmail.com>
2024-03-28 19:24:59 -0700

    Added temp darkmode for ease on the eyes

--- portfoliojs.client/src/main.jsx
+++ portfoliojs.client/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ResponsiveAppBar from '../src/Components/NavigationBar.jsx'
 import RowAndColumnSpacing from '../src/Components/CharacterStatements.jsx'
+import App from '../src/Components/DarkMode.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ResponsiveAppBar/>
     <RowAndColumnSpacing/>
+    <App/>
   </React.StrictMode>,
 )
