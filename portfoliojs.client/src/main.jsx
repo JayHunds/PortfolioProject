e5ba9b5dae29cd2c733134b2fcb1543d15948882
Jayson Hundsrucker <hundsruckerjayson@gmail.com>
2024-03-29 12:09:32 -0700

    Created RotatingCards file, aswell as HighlightedCode

--- portfoliojs.client/src/main.jsx
+++ portfoliojs.client/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import ResponsiveAppBar from '../src/Components/NavigationBar.jsx'
 import RowAndColumnSpacing from '../src/Components/CharacterStatements.jsx'
 import App from '../src/Components/DarkMode.jsx'
+import SpacingGrid from '../src/Components/RotatingCards.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ResponsiveAppBar/>
     <RowAndColumnSpacing/>
     <App/>
+    <SpacingGrid/>
   </React.StrictMode>,
 )
